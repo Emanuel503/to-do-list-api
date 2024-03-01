ec241dfc2058d282707c352d6f202e92bba597bd
emmanueljose.molina <emmanueljose.molina@salud.gob.sv>
2024-03-01 10:54:45 -0600

    Add routes task, users

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,11 @@
 <?php
 
+use App\Http\Controllers\TasksController;
+use App\Http\Controllers\ToDoController;
+use App\Http\Controllers\UsersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::resource('/user', UsersController::class);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::resource('/task', TasksController::class);
