30ea4c190a55653490c43ad1714faa4a92cbb551
emmanueljose.molina <emmanueljose.molina@salud.gob.sv>
2024-03-05 09:05:07 -0600

    Fix routes and add route

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,13 @@
 <?php
 
+use App\Http\Controllers\TasksCategoriesController;
 use App\Http\Controllers\TasksController;
 use App\Http\Controllers\UsersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('/user', UsersController::class);
+Route::resource('/users', UsersController::class);
 
-Route::resource('/task', TasksController::class);
+Route::resource('/tasks', TasksController::class);
+
+Route::resource('/categories', TasksCategoriesController::class);
