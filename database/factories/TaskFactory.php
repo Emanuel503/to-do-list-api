195a0dec69ad8da7ea38c28ab49d35a38f93ed83
Emanuel Molina <emmanueljose.molina@salud.gob.sv>
2024-04-12 11:27:56 -0600

    add seders, factories

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $categories = ['comida', 'animales', 'otros', 'tareas', null];
+
+        return [
+            'user_id_register'  => 1,
+            'id_task_status'    => 1,
+            'title'             => fake()->text(100),
+            'description'       => fake()->text(),
+            'description'       => fake()->text(),
+            'category'          => fake()->randomElement($categories),
+            'color'             => fake()->numberBetween(100000, 999999)
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             TaskStatusSeeder::class,
+            UserSeeder::class,
+            TasksSeeder::class,
         ]);
     }
 }
