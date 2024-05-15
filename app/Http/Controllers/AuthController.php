aac7bd75db0f909374f572f46a0fbf36d9b68f92
Emanuel Molina <emmanueljose.molina@salud.gob.sv>
2024-05-15 14:18:33 -0600

    Add swagger, basic configuration

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,7 +7,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
-
+/**
+* @OA\Info(title="API TASK", version="1.0")
+*
+*/
 class AuthController extends Controller
 {
     public function register(Request $request)
@@ -67,6 +70,49 @@ class AuthController extends Controller
         ], 201);
     }
 
+    /**
+     * @OA\Post(
+     *     path="/api/login",
+     *     tags={"Auth"},
+     *     summary="Login user and return a token",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             @OA\Property(property="email", type="string", example="emanueljosemolina@gmail.com"),
+     *             @OA\Property(property="password", type="string", example="password")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="code", type="number", example=200),
+     *             @OA\Property(property="message", type="string", example="Login success"),
+     *             @OA\Property(property="data", type="object",
+     *                 @OA\Property(property="access_token", type="string", example="eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjo..."),
+     *                 @OA\Property(property="token_type", type="string", example="Bearer"),
+     *                 @OA\Property(property="user", type="object",
+     *                       @OA\Property(property="id", type="integer", example="1"),
+     *                       @OA\Property(property="name", type="string", example="Emanuel Molina"),
+     *                       @OA\Property(property="email", type="string", example="emanueljosemolina@gmail.com"),
+     *                       @OA\Property(property="email_verified_at", type="string", example="2024-05-14 19:38:33"),
+     *                       @OA\Property(property="created_at", type="string", example="2024-05-14T19:38:33.000000Z"),
+     *                       @OA\Property(property="updated_at", type="string", example="2024-05-14T19:38:33.000000Z"),
+     *                 )
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Invalid credentials",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="code", type="number", example=401),
+     *             @OA\Property(property="message", type="string", example="Password or email is invalid"),
+     *             @OA\Property(property="data", type="object", example=null),
+     *         )
+     *     )
+     * )
+     */
     public function login(Request $request)
     {
         $rules = array(
@@ -94,7 +140,7 @@ class AuthController extends Controller
                 'code'      => 401,
                 'message'   => 'Password or email is invalid',
                 'data'      => null
-            ], 200);
+            ], 401);
 
         $user = $request->user();
         $tokenResult = $user->createToken('Personal Access Token');
