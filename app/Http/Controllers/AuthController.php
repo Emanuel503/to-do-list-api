d8190d3ffecaa921ea44a3987f841b1a459cd412
Emanuel Molina <emmanueljose.molina@salud.gob.sv>
2024-05-16 14:34:59 -0600

    Add documentation register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -21,13 +21,110 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+    /**
+     * @OA\Post(
+     *     path="/api/register",
+     *     tags={"Auth"},
+     *     summary="Register user and return a token",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\MediaType(
+     *             mediaType="multipart/form-data",
+     *             @OA\Schema(
+     *                  @OA\Property(
+     *                     property="email",
+     *                     description="Email of the user",
+     *                     type="string",
+     *                     example="emanueljose@gmail.com"
+     *                 ),
+     *                  @OA\Property(
+     *                     property="password",
+     *                     description="Password of the user",
+     *                     type="string",
+     *                     example="password"
+     *                 ),
+     *                  @OA\Property(
+     *                     property="name",
+     *                     description="Name of the user",
+     *                     type="string",
+     *                     example="Emanuel Molina"
+     *                 ),
+     *                  @OA\Property(
+     *                     property="image",
+     *                     description="Image file to upload",
+     *                     type="string",
+     *                     format="binary"
+     *                 )
+     *             ),
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="code", type="number", example=201),
+     *             @OA\Property(property="message", type="string", example="User created successfully"),
+     *             @OA\Property(property="data", type="object",
+     *                 @OA\Property(property="user", type="object",
+     *                       @OA\Property(property="id", type="integer", example="1"),
+     *                       @OA\Property(property="name", type="string", example="Emanuel Molina"),
+     *                       @OA\Property(property="email", type="string", example="emanueljosemolina@gmail.com"),
+     *                       @OA\Property(property="email_verified_at", type="string", example="2024-05-14 19:38:33"),
+     *                       @OA\Property(property="image", type="string", example="public/images/LVOfSH7m3NHo3g3nZ6dK2HYrk1t59Vn8FxHDeJxM.jpg"),
+     *                       @OA\Property(property="created_at", type="string", example="2024-05-14T19:38:33.000000Z"),
+     *                       @OA\Property(property="updated_at", type="string", example="2024-05-14T19:38:33.000000Z"),
+     *                 ),
+     *                 @OA\Property(property="access_token", type="string", example="eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjo..."),
+     *                 @OA\Property(property="token_type", type="string", example="Bearer")
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Invalid credentials",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="code", type="number", example=401),
+     *             @OA\Property(property="message", type="string", example="Password or email is invalid"),
+     *             @OA\Property(property="data", type="object", example=null),
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=422,
+     *         description="User created error",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="code", type="number", example=401),
+     *             @OA\Property(property="message", type="string", example="Password or email is invalid"),
+     *             @OA\Property(property="data", type="object",
+     *                 @OA\Property(property="errors", type="object",
+     *                     @OA\Property(property="email", type="array",
+     *                        @OA\Items(type="string", example="The email has already been taken.")
+     *
+     *                     ),
+     *                      @OA\Property(property="password", type="array",
+     *                         @OA\Items(type="string", example="password is required.")
+     *                     ),
+     *                      @OA\Property(property="name", type="array",
+     *                         @OA\Items(type="string", example="name is required.")
+     *                     ),
+     *                      @OA\Property(property="image", type="array",
+     *                         @OA\Items(type="string", example={
+     *                             "The image field must be an image.",
+     *                             "The image field must be a file of type: jpg, jpeg, png, gif."
+     *                         })
+     *                     )
+     *                 )
+     *             )
+     *         )
+     *     ),
+     *  )
+     */
     public function register(Request $request)
     {
         $rules = array(
             'email'         => 'required|string|email|unique:users',
             'password'      => 'required|string',
             'name'          => 'required|string',
-            'image'         => 'image|mimes:jpg,jpeg,png,gif|max:5000'
+            'image'         => 'nullable|image|mimes:jpg,jpeg,png,gif|max:5000'
         );
 
         $messages = array(
