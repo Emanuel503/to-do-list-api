f0dcdc7a3427f08d7acccb3be82e85966262fb7c
Emanuel Molina <emmanueljose.molina@salud.gob.sv>
2024-05-17 13:58:30 -0600

    Add documentation categories

--- app/Http/Controllers/TasksCategoriesController.php
+++ app/Http/Controllers/TasksCategoriesController.php
@@ -9,6 +9,46 @@ use Illuminate\Support\Facades\DB;
 
 class TasksCategoriesController extends Controller
 {
+    /**
+     * @OA\Get(
+     *     path="/api/categories",
+     *     security={{"passport":{}}},
+     *     tags={"Categories"},
+     *     summary="Get categories return a array of categories",
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="code", type="number", example=200),
+     *             @OA\Property(property="message", type="string", example="List of categories successfully"),
+     *             @OA\Property(property="data", type="object",
+     *                 @OA\Property(property="categories", type="array",
+     *                     @OA\Items(type="object",
+     *                           @OA\Property(property="category", type="integer", example="Comida"),
+     *                           @OA\Property(property="count", type="integer", example=1),
+     *                      )
+     *                 )
+     *             )
+     *         )
+     *     ),
+     *      @OA\Response(
+     *         response=401,
+     *         description="Error: Unauthorized",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", example="Unauthenticated"),
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=403,
+     *         description="Invalid credentials",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="code", type="number", example=403),
+     *             @OA\Property(property="message", type="string", example="Unauthorized"),
+     *             @OA\Property(property="data", type="object", example=null),
+     *         )
+     *     )
+     * )
+     */
     public function index(){
 
         $categories = Task::select('category', DB::raw('COUNT(*) as count'))
