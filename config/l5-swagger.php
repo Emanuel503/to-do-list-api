3f284ac32c4775521fbcd46aa378ccf204437b08
Emanuel Molina <emmanueljose.molina@salud.gob.sv>
2024-05-23 13:29:10 -0600

    change auth swagger

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -185,19 +185,27 @@ return [
                 */
 
                 /* Open API 3.0 support */
-                'passport' => [ // Unique name of security
-                    'type' => 'oauth2', // The type of the security scheme. Valid values are "basic", "apiKey" or "oauth2".
-                    'description' => 'Laravel passport oauth2 security.',
+                // 'passport' => [ // Unique name of security
+                //     'type' => 'oauth2', // The type of the security scheme. Valid values are "basic", "apiKey" or "oauth2".
+                //     'description' => 'Laravel passport oauth2 security.',
+                //     'in' => 'header',
+                //     'scheme' => 'https',
+                //     'flows' => [
+                //         "password" => [
+                //             "authorizationUrl" => config('app.url') . '/oauth/authorize',
+                //             "tokenUrl" => config('app.url') . '/oauth/token',
+                //             "refreshUrl" => config('app.url') . '/token/refresh',
+                //             "scopes" => []
+                //         ],
+                //     ],
+                // ],
+                'passport' => [
+                    'type' => 'http',
+                    'description' => 'Laravel passport token.',
+                    'name' => 'Authorization',
                     'in' => 'header',
-                    'scheme' => 'https',
-                    'flows' => [
-                        "password" => [
-                            "authorizationUrl" => config('app.url') . '/oauth/authorize',
-                            "tokenUrl" => config('app.url') . '/oauth/token',
-                            "refreshUrl" => config('app.url') . '/token/refresh',
-                            "scopes" => []
-                        ],
-                    ],
+                    'scheme' => 'bearer',
+                    'bearerFormat' => 'JWT', // o cualquier otro formato que uses
                 ],
                 /*'sanctum' => [ // Unique name of security
                     'type' => 'apiKey', // Valid values are "basic", "apiKey" or "oauth2".
@@ -218,7 +226,12 @@ return [
                         'write'
                     ],
                     */
-                    'passport' => []
+                    'passport' => [],
+                    'bearerAuth' => [
+                        'type' => 'http',
+                        'scheme' => 'bearer',
+                        'bearerFormat' => 'JWT', // o cualquier otro formato que uses
+                    ],
                 ],
             ],
         ],
