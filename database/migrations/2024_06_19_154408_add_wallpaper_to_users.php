5b463b88c6ab385ae0bed9cbe727b12b0c13edaa
Emanuel Molina <emmanueljose.molina@salud.gob.sv>
2024-06-21 10:22:04 -0600

    Add migration wallpaper user

---
+++ database/migrations/2024_06_19_154408_add_wallpaper_to_users.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('wallpaper')->nullable()->after('image')->comment("User set wallpaper");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('wallpaper');
+        });
+    }
+};
